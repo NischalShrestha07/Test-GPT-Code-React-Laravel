0b096bbe2f05d145f844d20aa8dca24f646974ef
Nischal <nischalshrestha800@gmail.com>
2024-06-19 13:19:49 +0545

    first commit

---
+++ newportal/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use App\Http\Controllers\PostController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+});
